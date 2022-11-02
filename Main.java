fa1cda9300d75872c4edbc9453f1f266d6a9feaf
rus317521 <rus317521@yandex.ru>
2022-11-02 15:06:13 +0500

    Added Main and Person

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Накопителтьный счет:");
+    }
+
+    /*public static List<Person> generateClients() {
+        List<Person> listPerson = new List();
+        Person person = new Person('Alsu', 'Shcherbakova', 3);
+        listPerson.add(person);
+
+
+    }*/
+}
---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    protected String name;
+    protected String surname;
+    protected int count;
+
+    public Person(String name, String surname, int count) {
+        this.name = name;
+        this.surname - surname;
+        this.count = count;
+
+    }
+
+
+}
