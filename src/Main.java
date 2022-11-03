b0b008381914d710ca21064e671cc07650810230
rus317521 <rus317521@yandex.ru>
2022-11-03 19:28:54 +0500

    Edit Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Queue;
 
 
 public class Main {
@@ -14,24 +13,27 @@ public class Main {
         listPerson.add(new Person("Vera", "Grishina", 4));
         return listPerson;
     }
+
     public static void main(String[] args) {
 
-        Queue<Person> queue = new PriorityQueue<>();
+        Queue<Person> queue = new LinkedList<>();
 
         LinkedList<Person> clients = generateClients();
        /* for(int i=0;i<clients.size();i++)
         {queue.add(clients[i]);}*/
-        for (Person person: clients
-             ) {  queue.add(person);
-            
+        for (Person person : clients
+        ) {
+            queue.add(person);
+
         }
-         while(true)
-         {
-            if(queue.isEmpty()) break;
-            Person personMinus = new Person(queue.peek().name,queue.peek().surname,queue.peek().count-1);
-            if(personMinus.count!=0) {queue.add(personMinus);}
+        while (true) {
+            if (queue.isEmpty()) break;
+            Person personMinus = new Person(queue.peek().name, queue.peek().surname, queue.peek().count - 1);
+            if (personMinus.count != 0) {
+                queue.add(personMinus);
+            }
             System.out.println(queue.peek().name + " " + queue.poll().surname + " Прокатился на оттракционе ");
-         }
+        }
 
     }
 }
